79686c942046d77e389acbb54700acef9e85f40f
daurnimator <quae@daurnimator.com>
2016-10-24 17:08:55 -0700

    tui/tput: Read + output init_file/reset_file don't just print the file name...

--- tui/tput.lua
+++ tui/tput.lua
@@ -2,6 +2,19 @@ local tui_terminfo = require "tui.terminfo"
 
 local default_terminfo = tui_terminfo.find()
 
+local function read_file(path)
+	local fd, err, errno = io.open(fd, "rb")
+	if not fd then
+		return nil, err, errno
+	end
+	local contents, err2, errno2 = fd:read("*a")
+	fd:close()
+	if not contents then
+		return nil, err2, errno2
+	end
+	return contents
+end
+
 local function init(fd, terminfo)
 	if fd == nil then
 		fd = io.stdout
@@ -9,17 +22,23 @@ local function init(fd, terminfo)
 	if terminfo == nil then
 		terminfo = default_terminfo
 	end
-	return fd:write(
-		terminfo.init_prog or "",
-		terminfo.init_1string or "",
-		terminfo.init_2string or "",
-		terminfo.clear_margins or "",
-		terminfo.set_left_margin or "",
-		terminfo.set_right_margin or "",
-		terminfo.clear_all_tabs or "",
-		terminfo.set_tab or "",
-		terminfo.init_file or "",
-		terminfo.init_3string or "")
+	local str = {
+		terminfo.init_prog or "";
+		terminfo.init_1string or "";
+		terminfo.init_2string or "";
+		terminfo.clear_margins or "";
+		terminfo.set_left_margin or "";
+		terminfo.set_right_margin or "";
+		terminfo.clear_all_tabs or "";
+		terminfo.set_tab or "";
+		"";
+		terminfo.init_3string or "";
+	}
+	local file = terminfo.init_file
+	if file then
+		str[9] = read_file(file) or ""
+	end
+	return fd:write(table.concat(str))
 end
 
 local function reset(fd, terminfo)
@@ -29,17 +48,23 @@ local function reset(fd, terminfo)
 	if terminfo == nil then
 		terminfo = default_terminfo
 	end
-	return fd:write(
-		terminfo.init_prog or "",
-		terminfo.reset_1string or terminfo.init_1string or "",
-		terminfo.reset_2string or terminfo.init_2string or "",
-		terminfo.clear_margins or "",
-		terminfo.set_left_margin or "",
-		terminfo.set_right_margin or "",
-		terminfo.clear_all_tabs or "",
-		terminfo.set_tab or "",
-		terminfo.reset_file or terminfo.init_file or "",
-		terminfo.reset_3string or terminfo.init_3string or "")
+	local str = {
+		terminfo.init_prog or "";
+		terminfo.reset_1string or terminfo.init_1string or "";
+		terminfo.reset_2string or terminfo.init_2string or "";
+		terminfo.clear_margins or "";
+		terminfo.set_left_margin or "";
+		terminfo.set_right_margin or "";
+		terminfo.clear_all_tabs or "";
+		terminfo.set_tab or "";
+		"";
+		terminfo.reset_3string or terminfo.init_3string or "";
+	}
+	local file = terminfo.reset_file or terminfo.init_file
+	if file then
+		str[9] = read_file(file) or ""
+	end
+	return fd:write(table.concat(str))
 end
 
 return {
