8b37e8ab7548c97a0a5fc20ad68a5bccd4b97cc7
daurnimator <quae@daurnimator.com>
2016-10-25 11:00:27 -0700

    examples/terminfo: Sort output

--- examples/terminfo.lua
+++ examples/terminfo.lua
@@ -8,11 +8,14 @@ else
 end
 
 print(names[#names])
+local lines = {}
 for k, v in pairs(caps) do
 	if type(v) == "string" then
 		v = string.format("%q", v):gsub("\n", "\\n")
 	else
 		v = tostring(v)
 	end
-	print(string.format("\t%s: %s", k, v))
+	lines[#lines+1] = string.format("\t%s: %s\n", k, v)
 end
+table.sort(lines)
+print(table.concat(lines))
